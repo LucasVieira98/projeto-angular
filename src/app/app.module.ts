e242ba57e77bcd5a497c7e49328717546fabf794
LucasVieira98 <lucasmatheus.adm98@gmail.com>
2020-10-27 00:12:02 -0300

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
